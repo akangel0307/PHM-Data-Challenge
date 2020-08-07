d0ad3ed70c77af16e9fc0735edb6320ef84e7afd
gdutthu <2194454302@qq.com>
2020-08-07 11:16:05 +0800

    init

--- data/jwl/data_utils.py
+++ data/jwl/data_utils.py
@@ -4,19 +4,27 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import os
 
+'''
+PATH="D:\\repos\\data\\2016 PHM Data Challenge\\2016 PHM DATA CHALLENGE CMP DATA SET\\"
+stage = "test"
+stage_x = 'CMP-data\\'+stage
+'''
 def load_dataframe(PATH, stage, stage_x):
     # get x and y from corresponding dirs
-    path = os.path.join(PATH, stage_x)          #路径拼接，得到文件路径：PATH\stage_x
+    path = os.path.join(PATH, stage_x)                                     #路径名拼接，得到文件路径：PATH\stage_x
     dataframe_x = pd.DataFrame()
     for file_name in os.listdir(path):
         dataframe = pd.read_csv(os.path.join(path, file_name))
         dataframe = dataframe.drop(columns=['TIMESTAMP'])
         dataframe_x = dataframe_x.append(dataframe,ignore_index=True)
     # dataframe_group_x = dataframe_x.groupby(['WAFER_ID','STAGE'])
-    y_path = os.path.join(PATH, "CMP-"+stage+"-removalrate.csv") #CMP-test-removalrate.csv
+    y_path = os.path.join(PATH, "CMP-"+stage+"-removalrate.csv")           #CMP-test-removalrate.csv
     dataframe_y = pd.read_csv(y_path)
-    dataframe_y = dataframe_y.loc[dataframe_y['AVG_REMOVAL_RATE'] <= 1000]
-    dataframe_y.hist('AVG_REMOVAL_RATE')
+
+    dataframe_y = dataframe_y.loc[dataframe_y['AVG_REMOVAL_RATE'] <= 1000] #大于1000的数据，可以认为是异常值，进行舍弃
+    dataframe_y.hist('AVG_REMOVAL_RATE')                                   #对这一列绘制分布直方图
+    # plt.hist(dataframe_y['AVG_REMOVAL_RATE'])                            #将这列数据绘制出来，这列数据明显的分为两段
+    # plt.show()
     return dataframe_x, dataframe_y
 
 def merge_x_y(dataframe_x, dataframe_y):
@@ -27,6 +35,7 @@ def abstract_statistics(dataframe_x, dataframe_y, statistics=['mean','std','min'
     # abstract statistics for virtual metrology
     # dataframe_x has dropped timestamps
     dataframe_group_x = dataframe_x.groupby(['WAFER_ID','STAGE'])
+
     dataframe_statistics = dataframe_group_x.agg(statistics)
     # dataframe_statistics = dataframe_statistics.drop(columns = [('AVG_REMOVAL_RATE','std'),('AVG_REMOVAL_RATE','min'),('AVG_REMOVAL_RATE','median'),('AVG_REMOVAL_RATE','max')])
     columns = dataframe_x.columns
@@ -70,8 +79,6 @@ def split_data_label(data):
     x = data[:,:-1]
     y = data[:,-1]
     return x, y
-
-
 
 if __name__ == "__main__":
     # 在D盘新建文件夹(命名为repos)，将data文件夹放入到该文件夹中
